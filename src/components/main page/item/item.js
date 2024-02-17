667f718c9024e32537ae3d3381311354884a9427
Matheus Ramazza <01ramazza@gmail.com>
2024-02-17 14:43:59 -0300

    add color to each rarity of item

--- src/components/main page/item/item.js
+++ src/components/main page/item/item.js
@@ -3,15 +3,63 @@ import "./styles.css";
 
 function Items() {
     
-    const url = 'https://www.steamwebapi.com/steam/api/inventory?key=NVEJ8DCC922B0VZT&steam_id=76561198073034521&currency=BRL';
-    
+    //const url = 'https://www.steamwebapi.com/steam/api/inventory?key=NVEJ8DCC922B0VZT&steam_id=76561198073034521&currency=BRL';
+    //const url = 'https://www.steamwebapi.com/steam/api/inventory?key=NVEJ8DCC922B0VZT&steam_id=76561198835900666&currency=BRL';
+    const url = 'https://www.steamwebapi.com/steam/api/inventory?key=NVEJ8DCC922B0VZT&steam_id=76561198994437904&currency=BRL';
+
+
     const [items, setItems] = useState({
         valor: [],
         imgURL: [],
         nome: [],
         raridade: []
     });
-        
+    
+    // purple #292337
+    // blue #212b3e
+    // light blue #212b41
+    // grey #212b41
+    // purple? #7f506f
+    // red #523d34
+
+    function getRarityColor(rarity, aplpha = 0.19) {
+        switch (rarity) {
+            // Guns
+            case 'contraband':
+                return `rgba(235, 75, 75, ${aplpha})`; // knife
+            case 'covert':
+                return `rgba(235, 75, 75, ${aplpha})`; // red
+            case 'classified':
+                return `rgba(211, 44, 230, ${aplpha})`; // pink
+            case 'restricted':
+                return `rgba(136, 71, 255, ${aplpha})`; // purple
+            case 'mil-spec grade':
+                return `rgba(75, 105, 255, ${aplpha})`; // blue
+            case 'industrial grade':
+                return `rgba(94, 152, 217, ${aplpha})`; // light blue
+            // Agents
+            case 'master':
+                return `rgba(235, 75, 75, ${aplpha})`; // red
+            case 'superior':
+                return `rgba(211, 44, 230, ${aplpha})`; // pink
+            case 'exceptional':
+                return `rgba(136, 71, 255, ${aplpha})`; // purple
+            case 'distinguished':
+                return `rgba(75, 105, 255, ${aplpha})`; // blue
+            // Stickers
+            case 'extraordinary':
+                return `rgba(211, 44, 230, ${aplpha})`; // pink
+            case 'exotic':
+                return `rgba(136, 71, 255, ${aplpha})`; // purple
+            case 'remarkable':
+                return `rgba(75, 105, 255, ${aplpha})`; // blue
+            case 'high grade':
+                return `rgba(94, 152, 217, ${aplpha})`; // light blue
+            // Grey items and cases
+            default:
+                return `rgba(176, 195, 217, ${aplpha})`; // default color(grey)
+        }
+    }
     
     useEffect(() => {
 
@@ -48,11 +96,13 @@ function Items() {
         <>
             <div className="item-wrapper">
                 {items.nome.map((itemName, index) => (
-                    <div className="item" key={index}>
+                    <div className="item" style={{ 
+                        backgroundImage: `radial-gradient(circle at 50% 100%, ${getRarityColor(items.raridade[index])}, transparent 150%)`,
+                        borderBottom: `5px solid ${getRarityColor(items.raridade[index], 0.5)}`}} 
+                    key={index}>
                         <h4 className="valor">{`R$` + items.valor[index]}</h4>
                         <img className="imagem" src={items.imgURL[index]} alt="" />
                         <h4 className="nome">{itemName}</h4>
-                        {/* <p className="raridade">{items.raridade[index]}</p> */}
                     </div>
                 ))}
             </div>
